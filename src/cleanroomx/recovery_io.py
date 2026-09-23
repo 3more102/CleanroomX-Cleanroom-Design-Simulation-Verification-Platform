0365f7f55716145a95040de405eae68c70bdfb4a
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-23 13:21:46 +0300

    v0.5: update src/cleanroomx/recovery_io.py

--- src/cleanroomx/recovery_io.py
+++ src/cleanroomx/recovery_io.py
@@ -14,9 +14,15 @@ def recovery_test_from_dict(data: dict) -> RecoveryTestSpec:
         target_concentration_per_m3=data["target_concentration_per_m3"],
         max_recovery_time_minutes=data.get("max_recovery_time_minutes"),
         instrument_id=data.get("instrument_id"),
+        instrument_serial_number=data.get("instrument_serial_number"),
+        calibration_certificate_id=data.get("calibration_certificate_id"),
+        calibration_date=data.get("calibration_date"),
+        calibration_due_date=data.get("calibration_due_date"),
         sample_location=data.get("sample_location"),
         occupancy_state=data.get("occupancy_state"),
         method_reference=data.get("method_reference"),
+        data_source=data.get("data_source"),
+        analyst=data.get("analyst"),
         samples=samples,
     )
 

6900144cfd27884fbffcb8509a2d1df2a72343d3
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-24 21:45:37 +0300

    test: carry latest v0.97 result-cache removal regression

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -208,6 +208,7 @@ def test_remove_analysis_invalidates_matching_result(monkeypatch):
     )
     app.last_run = object()
     app.last_run_analysis_id = "a"
+    app._runs_by_analysis = {"a": app.last_run}
     app.result_text = object()
     app.report_text = object()
     app.diagnostics_text = object()

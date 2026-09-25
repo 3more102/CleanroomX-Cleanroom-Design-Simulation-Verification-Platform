3967c0c4e23afe74ec2ab0090297f1ab948e042d
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-25 14:20:23 +0300

    Remove obsolete CLI Path import

--- src/cleanroomx/fan_uncertainty_cli.py
+++ src/cleanroomx/fan_uncertainty_cli.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import argparse
 import json
-from pathlib import Path
 
 from .persistence import atomic_write_text
 from .fan_uncertainty import analyze_fan_system_uncertainty
